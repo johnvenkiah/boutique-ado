38f747e617f256c4cfe3e1b8f7ff412a768a6099
johnvenkiah <contact@johnvenkiah.com>
2021-12-07 12:17:26 +0000

    home app url and view created and loads

---
+++ home/urls.py
@@ -0,0 +1,11 @@
+"""
+home URL Configuration
+"""
+
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,15 @@
+"""
+Tjene rowt
+"""
+
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    """
+    A view to display the home/index page
+    """
+
+    return render(request, 'home/index.html')

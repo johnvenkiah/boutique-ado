1f0e69b4cc40de5c058f88a6e5e1188656d469a4
johnvenkiah <contact@johnvenkiah.com>
2021-12-08 11:04:28 +0000

    customize admin

--- products/models.py
+++ products/models.py
@@ -8,6 +8,9 @@ class Category(models.Model):
     """
     Category class
     """
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 

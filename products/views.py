a59e04a29aefb6510c4db7825ad7278efea4a7ac
johnvenkiah <contact@johnvenkiah.com>
2021-12-10 00:17:33 +0000

    more on product size logic

--- products/views.py
+++ products/views.py
@@ -42,7 +42,7 @@ def all_products(request):
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You, didn't enter anything to search.")
+                messages.error(request, "You didn't enter any search criteria.")
                 return redirect(reverse('products'))
 
             queries = Q(name__icontains=query) | Q(description__icontains=query)

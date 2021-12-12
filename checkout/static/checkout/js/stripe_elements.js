a99108633028ef7f742695f7bb4991ec9d523af6
johnvenkiah <contact@johnvenkiah.com>
2021-12-12 22:42:34 +0000

    add stripe_elements.js and styling for stripe element

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -10,5 +10,22 @@ let stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
 let client_secret = $('#id_client_secret').text().slice(1, -1);
 let stripe = Stripe(stripe_public_key);
 let elements = stripe.elements();
-let card = elements.create('card');
+
+var style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconcolor: '#dc3545'
+    }
+};
+
+let card = elements.create('card', {style: style});
 card.mount('#card-element');

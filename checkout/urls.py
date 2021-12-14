4bef0ca4b1098b572846efd7a1635137b9415753
johnvenkiah <contact@johnvenkiah.com>
2021-12-14 03:25:36 +0000

    cofigure webhooks

--- checkout/urls.py
+++ checkout/urls.py
@@ -4,6 +4,7 @@ home URL Configuration
 
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
@@ -11,4 +12,5 @@ urlpatterns = [
         'checkout_success/<order_number>',
         views.checkout_success, name='checkout_success'
     ),
+    path('wh/', webhook, name='webhook'),
 ]

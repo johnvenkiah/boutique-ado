5d58f02f813c52c9154af56708a8b4da8de4b0fd
johnvenkiah <contact@johnvenkiah.com>
2021-12-15 14:15:34 +0000

    initial display orders in profile page

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+
 from .models import UserProfile
 from .forms import UserProfileForm
-from django.contrib import messages
+from checkout.models import Order
+
 
 def profile(request):
     """ Display users profile """
@@ -22,6 +25,20 @@ def profile(request):
         'form': form,
         'orders': orders,
         'on_profile_page': True,
+    }
+
+    return render(request, template, context)
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.info(request, (
+        f'This is a past confirmmation for order number {order_number}.'
+        'A confirmation email was sent on the order date'
+    ))
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)

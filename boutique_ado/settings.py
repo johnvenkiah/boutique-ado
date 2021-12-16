463c8985dd9d9e4ab5af924a8376d5fac60ea3e8
johnvenkiah <contact@johnvenkiah.com>
2021-12-16 15:26:14 +0000

    remove secret key from project

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eahpyr_7*44uhbrf6ihd6@jd=gl9w+j)_o26zk!*(hwqo*0x#*'
+SECRET_KEY = os.environ.get('SECRET_KEY_TWO', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['jvenkiah-boutique-ado.herokuapp.com', 'localhost']
 
